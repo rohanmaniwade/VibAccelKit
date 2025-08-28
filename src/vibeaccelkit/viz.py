74e93c1b26a16b63e38367cd1615978a93a217da
Rohan Maniwade <rohanmaniwade@gmail.com>
2025-08-28 15:26:43 +0200

    Power of 10 ticks on plots

--- src/vibeaccelkit/viz.py
+++ src/vibeaccelkit/viz.py
@@ -4,6 +4,29 @@ from scipy.integrate import trapezoid
 
 def _rms_from_psd(psd, f): return float(np.sqrt(trapezoid(psd, f)))
 
+def _decade_ticks(freqs):
+    """Return (tickvals, ticktext) at powers of 10 within freqs range."""
+    f = np.asarray(freqs, float)
+    f = f[f > 0]
+    if f.size == 0:
+        return None, None
+    fmin, fmax = np.min(f), np.max(f)
+    n0 = int(np.ceil(np.log10(fmin)))
+    n1 = int(np.floor(np.log10(fmax)))
+    vals = [10.0 ** k for k in range(n0, n1 + 1)]
+    labels = [f"{v:g}" for v in vals]
+    return vals, labels
+
+def _apply_log_axes(fig, logx, logy, freqs):
+    fig.update_layout(
+        xaxis_type="log" if logx else "linear",
+        yaxis_type="log" if logy else "linear",
+    )
+    if logx:
+        tickvals, ticktext = _decade_ticks(freqs)
+        if tickvals:
+            fig.update_xaxes(tickmode="array", tickvals=tickvals, ticktext=ticktext)
+
 def plot_fds(freqs, curves, title="FDS", logx=True, logy=True):
     fig = go.Figure()
     for label, arr in curves.items():
@@ -12,10 +35,8 @@ def plot_fds(freqs, curves, title="FDS", logx=True, logy=True):
         title=title,
         xaxis_title="Frequency [Hz]",
         yaxis_title="FDS",
-        template="plotly_white",
-        xaxis_type="log" if logx else "linear",
-        yaxis_type="log" if logy else "linear",
-    )
+        template="plotly_white")
+    _apply_log_axes(fig, logx, logy, freqs)
     return fig
 
 def plot_ers(freqs, curves, title="ERS", logx=True, logy=True):
@@ -27,10 +48,8 @@ def plot_ers(freqs, curves, title="ERS", logx=True, logy=True):
         title=title,
         xaxis_title="Frequency [Hz]",
         yaxis_title="ERS",
-        template="plotly_white",
-        xaxis_type="log" if logx else "linear",
-        yaxis_type="log" if logy else "linear",
-    )
+        template="plotly_white")
+    _apply_log_axes(fig, logx, logy, freqs)
     return fig
 
 def plot_srs(freqs, curves, title="SRS", logx=True, logy=True):
@@ -43,10 +62,8 @@ def plot_srs(freqs, curves, title="SRS", logx=True, logy=True):
         title=title,
         xaxis_title="Frequency [Hz]",
         yaxis_title="SRS amplitude",
-        template="plotly_white",
-        xaxis_type="log" if logx else "linear",
-        yaxis_type="log" if logy else "linear",
-    )
+        template="plotly_white")
+    _apply_log_axes(fig, logx, logy, freqs)
     return fig
 
 def plot_psd(freqs, curves, title="PSD", logx=True, logy=True):
@@ -59,8 +76,6 @@ def plot_psd(freqs, curves, title="PSD", logx=True, logy=True):
         title=title,
         xaxis_title="Frequency [Hz]",
         yaxis_title="PSD [(m/s^2)^2/Hz]",
-        template="plotly_white",
-        xaxis_type="log" if logx else "linear",
-        yaxis_type="log" if logy else "linear",
-    )
+        template="plotly_white")
+    _apply_log_axes(fig, logx, logy, freqs)
     return fig
